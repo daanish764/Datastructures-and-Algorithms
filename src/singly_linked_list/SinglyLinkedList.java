4d69f43d00af115626b9dec1d3f7ff772f1a6b7f
daanish764 <daanish764@hotmail.com>
2025-06-25 18:48:20 -0400

    SinglyLinkedList answer

--- src/singly_linked_list/SinglyLinkedList.java
+++ src/singly_linked_list/SinglyLinkedList.java
@@ -15,23 +15,103 @@ public class SinglyLinkedList {
 
 
     void insertAtBeginning(int data) {
-
-
+        if(startNode == null) {
+            startNode = new Node(data);
+        }
+        else {
+            Node n = new Node(data);
+            n.next = startNode;
+            startNode = n;
+        }
     }
 
-     void insertAtEnd(int data) {
+    void insertAtEnd(int data) {
 
+        if(startNode == null) {
+            startNode = new Node(data);
+            return;
+        }
+
+        Node n = startNode;
+        while(n.next != null) {
+            n = n.next;
+        }
+
+        n.next = new Node(data);
     }
 
     void insertAtPosition(int position, int value) {
 
+        if(position == 0) {
+            insertAtBeginning(value);
+            return;
+        }
+
+        Node current = startNode;
+        int index = 0;
+
+        while(current!=null && index < position-1) {
+            current = current.next;
+            index++;
+        }
+
+        if(current == null) {
+            System.out.println("Position out of bounds");
+            return;
+        }
+
+        Node newNode = new Node(value);
+        newNode.next = current.next;
+        current.next = newNode;
+
+
     }
 
     void display() {
+        Node n = startNode;
 
+        while(n != null) {
+            System.out.print(n.value + " ");
+            n = n.next;
+        }
+
+        System.out.println();
     }
 
     void deleteAtPosition(int position) {
+
+        if(startNode == null) {
+            System.out.println("List is empty. Cannot delete");
+            return;
+        }
+
+        if(position == 0) {
+            Node temp = startNode;
+            startNode = startNode.next;
+            temp.next = null;
+            return;
+        }
+
+
+
+        Node previous = startNode;
+        Node current = startNode.next;
+        int index = 1;
+
+        while(current!=null && index < position) {
+            previous = current;
+            current = current.next;
+            index++;
+        }
+
+        if(current==null) {
+            System.out.println("Position out of bound.");
+            return;
+        }
+
+        previous.next = current.next;
+        current.next = null;
+
 
     }
 
