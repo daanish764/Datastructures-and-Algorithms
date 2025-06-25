efc2a114a4f7b98a4d3d06c40f310a92659416ea
daanish764 <daanish764@hotmail.com>
2025-06-25 18:47:51 -0400

    SinglyLinkedList question

---
+++ src/singly_linked_list/SinglyLinkedList.java
@@ -0,0 +1,38 @@
+package singly_linked_list;
+
+class Node {
+    int value;
+    Node next;
+
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+public class SinglyLinkedList {
+
+    Node startNode;
+
+
+    void insertAtBeginning(int data) {
+
+
+    }
+
+     void insertAtEnd(int data) {
+
+    }
+
+    void insertAtPosition(int position, int value) {
+
+    }
+
+    void display() {
+
+    }
+
+    void deleteAtPosition(int position) {
+
+    }
+
+}
