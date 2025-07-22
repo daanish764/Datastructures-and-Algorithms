b4ee6e0694a51c54ebdae4d35ad8d96268e7c6bd
daanish764 <daanish764@hotmail.com>
2025-07-22 18:46:01 -0400

    Leetcode 121 Best Time to Buy and Sell Stock

---
+++ src/neetcode150/easy/N121BestTimetoBuyandSellStock.java
@@ -0,0 +1,38 @@
+package neetcode150.easy;
+
+public class N121BestTimetoBuyandSellStock {
+
+
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+
+
+    public static int maxProfit(int[] prices) {
+
+        if(prices.length<2) return 0;
+
+        int maxProfit = 0;
+
+        int buy = 0;
+        int sell = buy + 1;
+
+        while(sell < prices.length) {
+            if(prices[sell] < prices[buy]) {
+                buy = sell;
+            }
+            else {
+                int profit = prices[sell] - prices[buy];
+                maxProfit = Math.max(maxProfit, profit);
+            }
+
+            sell++;
+        }
+
+        return maxProfit;
+    }
+
+}
