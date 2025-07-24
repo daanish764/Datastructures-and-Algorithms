028e415922997cebc7abea7e424deb4ebe05616a
daanish764 <daanish764@hotmail.com>
2025-07-24 18:08:55 -0400

    Leetcode 104 Maximum Depth of Binary Tree (iterative BFS solution)

--- src/neetcode150/easy/N104_MaximumDepthOfBinaryTree.java
+++ src/neetcode150/easy/N104_MaximumDepthOfBinaryTree.java
@@ -1,5 +1,8 @@
 package neetcode150.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class N104_MaximumDepthOfBinaryTree {
 
     static class Node{
@@ -11,6 +14,25 @@ public class N104_MaximumDepthOfBinaryTree {
         Node(int val, Node left, Node right) {this.val = val; this.left = left; this.right = right;}
     }
 
+    // iterative breath first search
+    public static int maxDepthBFS(Node root) {
+        Queue<Node> queue = new LinkedList<Node>();
+
+        queue.add(root);
+        int levels = 0;
+        while(!queue.isEmpty()) {
+            int elementsAtLevel = queue.size();
+            for(int i=0; i<elementsAtLevel; i++) {
+                Node n = queue.poll();
+                if(n.left != null) {queue.add(n.left);}
+                if(n.right != null) {queue.add(n.right);}
+            }
+            levels++;
+
+        }
+
+        return levels;
+    }
 
     // recursive solution
     public static int maxDepth(Node node) {
@@ -28,6 +50,7 @@ public class N104_MaximumDepthOfBinaryTree {
         Node tree = new Node(3, new Node(9), new Node(20, new Node(15), new Node(7)));
 
         System.out.println(maxDepth(tree));
+        System.out.println(maxDepthBFS(tree));
 
     }
 }
